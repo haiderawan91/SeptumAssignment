c963e348cb9defd6988ce685d16ee8d396fa7c15
Haider Awan <haiderawan91@gmail.com>
2024-06-12 23:00:47 +0500

    created express server

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173' // Allow requests from this origin
+}));
+app.use(routes);
+
+try {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+} catch (error) {
+    console.log(error);
+}
+
